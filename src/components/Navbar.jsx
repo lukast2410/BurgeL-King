5f7a0b1a3ae6e9a62dfaaa7b9502ed553b4870c0
lukast2410 <lt24k24@gmail.com>
2021-10-16 10:30:05 +0700

    Carousel & Header

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,30 @@
+import { Link } from 'react-router-dom'
+import styles from '../styles/components/Navbar.module.scss'
+
+export default function Navbar() {
+	return (
+		<div className={styles.container}>
+			<nav className={styles.navbar}>
+				<div className={styles.logo}>
+					<Link to='/'>
+						<img src='/logo-land.png' alt='BurgeL King' />
+					</Link>
+				</div>
+				<div className={styles.navMenu}>
+					<Link to='/'>
+						<div>Menu</div>
+					</Link>
+					<Link to='/'>
+						<div>Order</div>
+					</Link>
+					<Link to='/'>
+						<div>Coming Soon</div>
+					</Link>
+					<Link to='/'>
+						<div>About us</div>
+					</Link>
+				</div>
+			</nav>
+		</div>
+	)
+}
---
+++ src/components/home/Slider.jsx
@@ -0,0 +1,95 @@
+import styles from '../../styles/components/Slider.module.scss'
+import { ChevronRightIcon, ChevronLeftIcon } from '@heroicons/react/outline'
+import { useEffect } from 'react'
+import $ from 'jquery'
+
+export default function Slider() {
+	useEffect(() => {
+		$(function () {
+			let width = '100%'
+			let animationSpeed = 1000
+			let pause = 4000
+			let curr = 1
+			let sliding = false
+
+			let burgerSlider = $(`.${styles.burgerSlider}`)
+			let listSlide = burgerSlider.find(`.${styles.listPromos}`)
+			let promoImageList = listSlide.find(`.${styles.burgerPromo}`)
+
+			function start() {
+				setInterval(function () {
+					sliding = true
+					listSlide.animate({ 'margin-left': '-=' + width }, animationSpeed, function () {
+						sliding = false
+						curr++
+						if (curr >= promoImageList.length) {
+							curr = 1
+							listSlide.css('margin-left', 0)
+						}
+					})
+				}, pause)
+			}
+			start()
+
+			let next = $(`#next`)
+			let prev = $(`#prev`)
+
+			next.on('click', function () {
+				if (!sliding) {
+					if (curr < promoImageList.length) curr++
+					listSlide.animate({ 'margin-left': '-=' + width }, 500)
+					if (curr >= promoImageList.length) {
+						curr = 1
+						listSlide.css('margin-left', 0)
+					}
+				}
+			})
+
+			prev.on('click', function () {
+				if (!sliding) {
+					if (curr > 0) curr--
+					listSlide.css('margin-left', '+=' + width)
+					if (curr === 1) {
+						curr = promoImageList.length
+						listSlide.css('margin-left', '-400%')
+					} else if (curr < 1) {
+						curr = promoImageList.length - 1
+						listSlide.css('margin-left', '-300%')
+					}
+				}
+			})
+		})
+	}, [])
+
+	return (
+		<div className={styles.burgerSlider}>
+			<ul className={styles.listPromos}>
+				<li className={styles.burgerPromo}>
+					<img src='/assets/banner1.jpg' alt='Banner 1' />
+				</li>
+				<li className={styles.burgerPromo}>
+					<img src='/assets/banner2.jpg' alt='Banner 2' />
+				</li>
+				<li className={styles.burgerPromo}>
+					<img src='/assets/banner3.jpg' alt='Banner 3' />
+				</li>
+				<li className={styles.burgerPromo}>
+					<img src='/assets/banner4.jpg' alt='Banner 4' />
+				</li>
+				<li className={styles.burgerPromo}>
+					<img src='/assets/banner1.jpg' alt='Banner 1' />
+				</li>
+			</ul>
+			<div className={`${styles.sliderBtn}`}>
+				<button id='prev' type='button'>
+					<ChevronLeftIcon className={`${styles.sliderBtnIcon} ${styles.prevIcon}`} />
+				</button>
+			</div>
+			<div className={`${styles.sliderBtn}`}>
+				<button id='next' type='button'>
+					<ChevronRightIcon className={`${styles.sliderBtnIcon} ${styles.nextIcon}`} />
+				</button>
+			</div>
+		</div>
+	)
+}
